5de28d081d726ed279b91891793e0535d995b528
Sven Fink <sven.fink@wipotec.com>
2018-03-28 14:08:22 +0200

    Allow Authorization and Post on header

--- components/response.php
+++ components/response.php
@@ -3,7 +3,7 @@
 class JsonProtocol{
   public static function sendResponse($data){
     $json = json_encode($data);
-    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, Post");
     header("Access-Control-Allow-Origin: *");
     header('Content-Type: application/json');
     echo $json;
