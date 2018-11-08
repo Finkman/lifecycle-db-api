d3de5565bc29d86b0edf1069f58963582a10a404
Sven Fink <sven.fink@wipotec.com>
2018-11-08 09:14:34 +0100

    Extend interface to request one single project

--- projects.php
+++ projects.php
@@ -5,9 +5,16 @@ require_once($_SERVER['DOCUMENT_ROOT']."/api/components/authorization.php");
 
 Auth::restrictAccess();
 
+if(isset($_GET['id'])){
+  $id = urldecode($_GET['id']);
+  $whereClause = "WHERE id = ".$id;
+}else{
+  $whereClause = "WHERE 1";
+}
+
 $sql = mysqli_connect(config\DB_HOST, config\DB_USER, config\DB_PASS, config\DB_NAME);
 
-$query = "SELECT * FROM projects";
+$query = "SELECT * FROM projects ".$whereClause;
 
 $result = mysqli_query($sql, $query);
 
